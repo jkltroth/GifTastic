fc302111f018f78629fb20e84b7ed094b66b7487
James Roth <jkltroth@gmail.com>
2017-10-29 22:32:31 -0400

    MVP final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,26 @@
 //Initial array of movies
 var tvShows = ["The Walking Dead", "Bob's Burgers", "Rick and Morty", "Breaking Bad", "Family Guy", "The Office", "Parks and Recreation", "Modern Family", "Seinfeld", "It's Always Sunny in Philadelphia"];
 
+//Function to display gifs
 function displayShowInfo() {
-
+    //Start with an empty gifs div
     $("#tvShowsGifDiv").empty();
-
+    
+    //Assign the data-name value of the button clicked to the variable 'show'
     var show = $(this).attr("data-name");
+    //Assign the giphy api URL to the variable 'queryURL'
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         show + "&api_key=dc6zaTOxFJmzC&limit=10";
-
+    //AJAX call to query the gihpy api using the 'queryURL' variable, and run the following function when data is returned...
     $.ajax({
         url: queryURL,
         method: "GET"
     }).done(function (response) {
 
+        //Assign the results of the query to 'results' variable
         var results = response.data;
 
+        //For loop to create gifs...
         for (var i = 0; i < results.length; i++) {
 
             var gifDiv = $("<div class='gifDiv'>");
@@ -36,11 +41,11 @@ function displayShowInfo() {
 
             $("#tvShowsGifDiv").prepend(gifDiv);
 
-        }
-
+        };
     });
 };
 
+//Function to play/pause gifs
 function changeStateOfGif() {
 
     var state = $(this).attr('data-state');
@@ -56,24 +61,20 @@ function changeStateOfGif() {
         $(this).attr('data-state', 'still')
     
     };
-
-    console.log($(this).attr('src'));
-
 };
 
+//Function to create tvShow buttons
 function renderButtons() {
 
     // Deleting the all buttons prior to adding new buttons
-    // (this is necessary otherwise you will have repeat buttons)
     $("#tvShowButtonsDiv").empty();
 
-    // Looping through the array of movies
+    // Looping through the array of shows
     for (var i = 0; i < tvShows.length; i++) {
 
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        // Then dynamicaly generating buttons for each show in the array
         var a = $("<button>");
-        // Adding a class of movie to our button
+        // Adding a class of show to our button
         a.addClass("btn btn-dark tvShowButton");
         // Adding a data-attribute
         a.attr("data-name", tvShows[i]);
@@ -101,9 +102,9 @@ $("#tvShowAdd").on("click", function (event) {
     $("#tvShowInput").val('');
 });
 
-// Adding a click event listener to all elements with a class of "movie"
+//Adding a click event listener to call displayShowInfo function when an element with a class of 'tvShowButton' is clicked
 $(document).on("click", ".tvShowButton", displayShowInfo);
-
+//Adding a click event listener to call changeStateOfGif function when an element with a class of 'gif' is clicked
 $(document).on("click", ".gif", changeStateOfGif);
-
+//Call the renderButtons function to create initial buttons
 renderButtons();
